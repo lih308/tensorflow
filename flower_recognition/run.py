7711c171ea35ee691301b73c03a142d386ce237b
lhe <lhe@wayfair.com>
2018-07-20 17:38:01 -0400

    Add missed files; update gitignore

---
+++ flower_recognition/run.py
@@ -0,0 +1,29 @@
+from pdb import set_trace as bp
+
+import numpy as np
+
+from model import *
+from util import *
+
+
+def run():
+    x_train, y_train, x_test, y_test = load_dataset()
+    results = model(
+        X_train=x_train,
+        Y_train=y_train,
+        X_test=x_test,
+        Y_test=y_test,
+    )
+
+
+def test_split_train_test_data():
+    data = {
+        'daisy': np.random.rand(100, 64, 64, 3),
+        'lily': np.random.rand(100, 64, 64, 3),
+        'rose': np.random.rand(100, 64, 64, 3),
+    }
+    split_train_test_data(data)
+
+
+if __name__ == '__main__':
+    run()
